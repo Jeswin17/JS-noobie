02b2fed59dd19306ab568160e3b4f4feea251046
Jeswin Jose <shield178@gmail.com>
2020-05-14 01:41:30 +0800

    MASS UPLOAD lol

---
+++ udemy.js
@@ -0,0 +1,28 @@
+<html>
+ 
+<head>
+    <title></title>
+</head>
+ 
+<body>
+    <script>
+ 
+        /*
+        var obj1 = new Object();
+        obj1.color = "red";
+        obj1.make = "ford";
+        console.log(obj1);
+        */
+        
+        var car = {color:"red",make:"ford",doors:5,model:"mustang"};  // literal
+        var prop = prompt("what did you want to know about the car");
+        document.write(car[prop]);
+        
+        
+        
+ 
+        
+    </script>
+</body>
+ 
+</html>
